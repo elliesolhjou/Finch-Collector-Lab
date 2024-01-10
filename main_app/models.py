628481ba55e782e2b2e81e610cc4f4bbf25a4460
Ellie Solhjou <elliesolhjou@gmail.com>
2024-01-10 14:58:04 -0600

    Model is set

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name=models.CharField(max_length=100)
+    breed=models.CharField(max_length=100)
+    threats=models.TextField(max_length=255)
+    habitat=models.TextField(max_length=255, null=True)
+
+    def __str__(self):
+        return f'{(self.name)} has {self.breed} breed'
